ed453553cfbd9b27f369d1dd98eac512bc477bb1
Miiick <mickwarehime@gmail.com>
2018-12-16 11:54:39 -0800

    initial commit

---
+++ src/app.py
@@ -0,0 +1,45 @@
+import logging
+import os
+import sys
+
+VERSION = '0.0'
+LOG_LEVEL = 'INFO'
+
+
+def clear_log(log_file: str) -> None:
+    if os.path.isfile(log_file):
+        f = open(log_file, 'r+')
+        f.truncate(0)
+    else:
+        make_log(log_file)
+
+
+def make_log(log_file: str) -> None:
+    basedir = os.path.dirname(log_file)
+    if not os.path.exists(basedir):
+        os.makedirs(basedir)
+    open(log_file, 'a').close()
+
+
+def initialize_logging() -> None:
+    log_file = 'src/logging/game.log'
+    clear_log(log_file)
+
+    fmt = '%(asctime)s {} [%(levelname)s]  %(message)s'.format(VERSION)
+    formatter = logging.Formatter(fmt)
+    logger = logging.getLogger()
+    logger.setLevel(LOG_LEVEL)
+
+    file_logger = logging.FileHandler(log_file)
+    file_logger.setFormatter(formatter)
+    logger.addHandler(file_logger)
+
+    console_logger = logging.StreamHandler()
+    console_logger.setFormatter(formatter)
+    logger.addHandler(console_logger)
+
+
+if __name__ == '__main__':
+    initialize_logging()
+    logging.info('Start Application')
+    sys.exit()
---
+++ src/tests/__init__.py
@@ -0,0 +1,1 @@
+# Needed for the tests to run
