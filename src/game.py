df117a1f74d3dbb47d55eb60412cd1a876dcfe9e
Dvir Kafri <dvir.kafri@gmail.com>
2018-12-24 14:31:09 -0500

    Small style/design cleanup for Game() (#20)
    * Make internal Game methods private.
    
    * Add class docstring.

--- src/game.py
+++ src/game.py
@@ -24,6 +24,7 @@ class GameState(Enum):
 # TODO(mick): create player state
 
 class Game(EventListener):
+    """Loads scenes, stores world, and handles framerate and quit event."""
     keyboard: Keyboard = None
     event_manager: EventManager = None
     controller: Controller = None
@@ -32,7 +33,7 @@ class Game(EventListener):
     def __init__(self) -> None:
         self.event_manager = EventManager()
         super(Game, self).__init__(self.event_manager)
-        self.initialize_pygame()
+        self._initialize_pygame()
 
         self.clock: pygame.Clock = pygame.time.Clock()
         self.screen: pygame.Surface = pygame.display.set_mode(
@@ -46,7 +47,7 @@ class Game(EventListener):
         # Change controller to change what is shown on the screen.
         self.controller = None
         self.prev_controller = None
-        self.new_game()
+        self._new_game()
 
     def notify(self, event: Event) -> None:
         if event == Event.QUIT:
@@ -57,35 +58,35 @@ class Game(EventListener):
 
             self.clock.tick(constants.FRAMES_PER_SECOND)
         elif event == Event.SETTINGS:
-            self.toggle_settings()
+            self._toggle_settings()
         elif event == Event.NEW_SCENE:
-            self.set_next_scene()
+            self._set_next_scene()
 
     def run(self) -> None:
         while True:
             self.event_manager.post(Event.TICK)
 
-    def initialize_pygame(self) -> None:
+    def _initialize_pygame(self) -> None:
         pygame.mixer.pre_init(44100, -16, 4, 2048)
         pygame.init()
         pygame.font.init()
 
-    def toggle_settings(self) -> None:
+    def _toggle_settings(self) -> None:
         if isinstance(self.controller, SettingsController):
-            self.remove_controller(self.controller)
+            self._remove_controller(self.controller)
             self.controller = self.prev_controller
         else:
             self.prev_controller = self.controller
             self.controller = SettingsController(self.event_manager, self.screen)
 
-    def new_game(self) -> None:
+    def _new_game(self) -> None:
         self.controller = LaunchController(self.event_manager, self.screen)
 
-    def set_next_scene(self) -> None:
-        self.remove_controller(self.controller)
+    def _set_next_scene(self) -> None:
+        self._remove_controller(self.controller)
         self.controller = self.scene_machine.build_scene(
             self.world, self.event_manager, self.screen)
 
-    def remove_controller(self, controller: Controller) -> None:
+    def _remove_controller(self, controller: Controller) -> None:
         self.controller.unregister()
         del controller
