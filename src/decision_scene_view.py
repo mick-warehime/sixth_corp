9369eb0b95413be66bb4c8388a533752fdca9f87
dvirkafri <dvir.kafri@gmail.com>
2018-12-26 15:27:19 +0000

    Simplify DecisionSceneView

--- src/decision_scene_view.py
+++ src/decision_scene_view.py
@@ -6,6 +6,15 @@ from typing import Dict
 from typing import List
 
 
+def _format_text(description: str,
+                 option_texts: List[str]) -> List[str]:
+    lines = description.split('\n') + option_texts
+    adjusted_lines: List[str] = []
+    for line in lines:
+        adjusted_lines += wrap(line, DecisionSceneView.WIDTH)
+    return adjusted_lines
+
+
 class DecisionSceneView(PygameView):
     WIDTH = 50
 
@@ -13,26 +22,8 @@ class DecisionSceneView(PygameView):
                  main_text: str, options: Dict[str, str]) -> None:
         super(DecisionSceneView, self).__init__(screen)
 
-        self.name = main_text
-        option_text = self.create_option_text(options)
-        self.texts = self.format_text(main_text, option_text)
-        print(self.texts)
+        option_text = ['{}: {}'.format(k, v) for k, v in options.items()]
+        self.texts = _format_text(main_text, option_text)
 
     def render(self) -> None:
         self.render_text()
-
-    def create_option_text(self,
-                           options: Dict[str, str]) -> List[str]:
-        texts = []
-        for option_key, description in options.items():
-            text = '{}: {}'.format(option_key, description)
-            texts.append(text)
-        return texts
-
-    def format_text(self, description: str,
-                    option_texts: List[str]) -> List[str]:
-        lines = description.split('\n') + option_texts
-        adjusted_lines: List[str] = []
-        for line in lines:
-            adjusted_lines += wrap(line, DecisionSceneView.WIDTH)
-        return adjusted_lines
