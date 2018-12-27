a7d522c22cf4cdcfa8bac03932e8006d67545c8b
dvirkafri <dvir.kafri@gmail.com>
2018-12-27 18:50:59 +0000

    Merge DecisionSceneController back with SceneController.

--- src/decision_scene_controller.py
+++ src/decision_scene_controller.py
@@ -1,17 +1,23 @@
+from conditions import IsDead
+from controller import Controller
 from decision_scene import DecisionScene
 from decision_scene_view import DecisionSceneView
-from events import InputEvent
+from event_utils import post_scene_change
+from events import InputEvent, EventType, Event
 from pygame import Surface
 
-from scene_controller import SceneController
+from scene_examples import game_over
 from world import World
 
 
-class DecisionSceneController(SceneController):
+class DecisionSceneController(Controller):
 
     def __init__(self, screen: Surface, world: World,
                  scene: DecisionScene) -> None:
-        super(DecisionSceneController, self).__init__(screen, world, scene)
+        super().__init__(screen)
+        self.world = world
+        self.scene = scene
+
         options = {key_val: choice.description
                    for key_val, choice in scene.choices.items()}
         self.view = DecisionSceneView(self.screen, scene.prompt, options)
@@ -19,3 +25,20 @@ class DecisionSceneController(SceneController):
     def _handle_input(self, input_event: InputEvent) -> None:
         if input_event.key in self.scene.choices:
             self.scene.make_choice(input_event.key)
+
+    def notify(self, event: EventType) -> None:
+        if not self._active:
+            return
+        if event == Event.TICK:
+            self.view.render()
+            if self.scene.is_resolved():
+                resolution = self.scene.get_resolution()
+                for effect in resolution.effects:
+                    effect.execute(self.world)
+
+                if IsDead().check(self.world.player):
+                    post_scene_change(game_over(self.world))
+                    return
+                post_scene_change(resolution.next_scene(self.world))
+        elif isinstance(event, InputEvent):
+            self._handle_input(event)
