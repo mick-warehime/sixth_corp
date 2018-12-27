ee0c310c2436fa71d24adcb5d9c23aca7478d476
dvirkafri <dvir.kafri@gmail.com>
2018-12-27 15:56:01 +0000

    remove unused classes

--- src/action_base.py
+++
@@ -1,8 +0,0 @@
-"""Basic interface for interacting with Characters."""
-from character_base import Character
-
-
-class Action(object):
-
-    def apply(self, character: Character) -> None:
-        raise NotImplementedError
