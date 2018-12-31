284e9a07e02ada56bce37d21f6c4edd1ed3d62f9
Miiick <mickwarehime@gmail.com>
2018-12-31 09:21:31 -1000

    make sure all tests run

--- src/models/effects_test.py
+++ src/models/effects_test.py
@@ -3,36 +3,31 @@ from models.effects import RestartWorld, IncrementAttribute, AcquireMod
 from models.mod_examples import HelmOfBeingOnFire
 from models.player import Player
 from models.states import Attribute
-from models.world import World
 
 
 def test_restart_world():
-    world = World()
-    player = world.player
-    RestartWorld().execute(world)
-    assert player is not world.player
+    player = Player()
+    RestartWorld().execute()
+    assert player is not Player()
 
 
 def test_increment_player_attribute():
-    world = World()
-    health = world.player.get_attribute(Attribute.HEALTH)
+    health = Player().get_attribute(Attribute.HEALTH)
     delta = -2
-    IncrementAttribute(Player(), Attribute.HEALTH, delta).execute(world)
-    assert world.player.get_attribute(Attribute.HEALTH) == health + delta
+    IncrementAttribute(Player(), Attribute.HEALTH, delta).execute()
+    assert Player().get_attribute(Attribute.HEALTH) == health + delta
 
 
 def test_increment_attribute():
-    world = World()
     health = 10
     char = Character(health=health)
     delta = -3
-    IncrementAttribute(char, Attribute.HEALTH, delta).execute(world)
+    IncrementAttribute(char, Attribute.HEALTH, delta).execute()
     assert char.get_attribute(Attribute.HEALTH) == health + delta
 
 
 def test_acquire_mod():
-    world = World()
     mod = HelmOfBeingOnFire()
-    assert mod not in world.player.inventory.all_mods()
-    AcquireMod(mod).execute(world)
-    assert mod in world.player.inventory.all_mods()
+    assert mod not in Player().all_mods()
+    AcquireMod(Player(), mod).execute()
+    assert mod in Player().all_mods()
--- src/models/world.py
+++ src/models/world.py
@@ -1,10 +1,4 @@
-from models.character_base import Character
-
-
 class World(object):
 
     def __init__(self) -> None:
-        self.player = Character(health=15)
-
-    def reset(self) -> None:
-        self.__init__()  # type: ignore
+        self._level = 0
