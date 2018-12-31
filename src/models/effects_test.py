61010cb2497289e926db3e60f57518e00a2ff54a
dvirk <dvir.kafri@gmail.com>
2018-12-31 17:45:15 +0000

    Update test.

--- src/models/effects_test.py
+++ src/models/effects_test.py
@@ -1,3 +1,5 @@
+import pytest
+
 from models.character_base import Character
 from models.effects import RestartWorld, IncrementPlayerAttribute, \
     IncrementAttribute, AcquireMod
@@ -5,24 +7,32 @@ from models.mod_examples import HelmOfBeingOnFire
 from models.states import Attribute
 from models.world import World
 
+_world = World()
 
-def test_restart_world():
-    world = World()
+
+@pytest.fixture(scope='function')
+def world():
+    return _world
+
+
+def teardown_function(function):
+    _world.reset()
+
+
+def test_restart_world(world):
     player = world.player
     RestartWorld().execute(world)
     assert player is not world.player
 
 
-def test_increment_player_attribute():
-    world = World()
+def test_increment_player_attribute(world):
     health = world.player.get_attribute(Attribute.HEALTH)
     delta = -2
     IncrementPlayerAttribute(Attribute.HEALTH, delta).execute(world)
     assert world.player.get_attribute(Attribute.HEALTH) == health + delta
 
 
-def test_increment_attribute():
-    world = World()
+def test_increment_attribute(world):
     health = 10
     char = Character(health=health)
     delta = -3
@@ -30,8 +40,7 @@ def test_increment_attribute():
     assert char.get_attribute(Attribute.HEALTH) == health + delta
 
 
-def test_acquire_mod():
-    world = World()
+def test_acquire_mod(world):
     mod = HelmOfBeingOnFire()
     assert mod not in world.player.inventory.all_mods()
     AcquireMod(mod).execute(world)
