2347065dc0308776052be883180ce73af999e6f5
dvirk <dvir.kafri@gmail.com>
2019-01-01 20:56:52 +0000

    remove circular import.

--- src/models/inventory.py
+++ src/models/inventory.py
@@ -2,32 +2,31 @@
 import abc
 from typing import Iterable, Callable
 
-from models.mods_base import Mod
 from models.states import AttributeType, State
 
 
 class InventoryBase(metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
-    def can_store(self, mod: Mod) -> bool:
+    def can_store(self, mod: 'Mod') -> bool:
         """Whether a mod can be stored."""
 
     @abc.abstractmethod
-    def store(self, mod: Mod) -> None:
+    def store(self, mod: 'Mod') -> None:
         """Store a mod.
 
         can_store should be called first.
         """
 
     @abc.abstractmethod
-    def remove(self, mod: Mod) -> None:
+    def remove(self, mod: 'Mod') -> None:
         """Remove a mod from the inventory."""
 
     @abc.abstractmethod
-    def all_mods(self) -> Iterable[Mod]:
+    def all_mods(self) -> Iterable['Mod']:
         """Iterate over all mods in storage."""
 
-    def mods(self, check: Callable[[Mod], bool]) -> Iterable[Mod]:
+    def mods(self, check: Callable[['Mod'], bool]) -> Iterable['Mod']:
         """All mods satisfying a given boolean function."""
         return (m for m in self.all_mods() if check(m))
 
@@ -48,15 +47,15 @@ class BasicInventory(InventoryBase):
     def __init__(self):
         self._mods = []
 
-    def can_store(self, mod: Mod) -> bool:
+    def can_store(self, mod: 'Mod') -> bool:
         return True
 
-    def store(self, mod: Mod) -> None:
+    def store(self, mod: 'Mod') -> None:
         self._mods.append(mod)
 
-    def remove(self, mod: Mod) -> None:
+    def remove(self, mod: 'Mod') -> None:
         assert mod in self._mods, 'mod {} not in inventory.'.format(mod)
         self._mods.remove(mod)
 
-    def all_mods(self) -> Iterable[Mod]:
+    def all_mods(self) -> Iterable['Mod']:
         return (m for m in self._mods)
