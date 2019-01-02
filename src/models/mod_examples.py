524e6eb7265efae79b453dc3e1ca99427dc438c0
dvirk <dvir.kafri@gmail.com>
2019-01-02 20:06:31 +0000

    Implement GenericMod

--- src/models/mod_examples.py
+++ src/models/mod_examples.py
@@ -2,7 +2,7 @@
 from typing import Dict, Sequence
 
 from models.abilities_base import Ability
-from models.ability_examples import FireLaser
+from models.ability_examples import FireLaser, Repair
 from models.mods_base import Mod
 from models.states import AttributeType, State, Attribute, Skill
 
@@ -74,3 +74,25 @@ class BasicLaser(Mod):
 
     def abilities_granted(self) -> Sequence[Ability]:
         return self._ability,
+
+
+class GenericMod(Mod):
+
+    def __init__(self, states_granted: Sequence[State] = (),
+                 attribute_modifiers: Dict[AttributeType, int] = None,
+                 abilities_granted: Sequence[Ability] = ()):
+        if attribute_modifiers is None:
+            attribute_modifiers = {}
+
+        self._states = states_granted
+        self._attr_mods = attribute_modifiers
+        self._abilities = abilities_granted
+
+    def states_granted(self) -> Sequence[State]:
+        return self._states
+
+    def attribute_modifiers(self) -> Dict[AttributeType, int]:
+        return self._attr_mods
+
+    def abilities_granted(self) -> Sequence[Ability]:
+        return self._abilities
