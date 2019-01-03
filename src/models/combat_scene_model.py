92c17dbd58feefe56c699c31083df29ba9361973
dvirk <dvir.kafri@gmail.com>
2019-01-03 04:24:44 +0000

    Get targetting working (not using moves).

--- src/models/combat_scene_model.py
+++ src/models/combat_scene_model.py
@@ -1,5 +1,8 @@
 import logging
+from itertools import product
+from typing import Tuple, List
 
+from models.abilities_base import Ability
 from models.character_base import Character
 from models.player import get_player
 from scenes.combat_scene import CombatScene
@@ -14,6 +17,8 @@ class CombatSceneModel(object):
 
     def __init__(self, scene: CombatScene) -> None:
         self.scene = scene
+        self._selected_ability: Ability = None
+        self._all_targets = (get_player(), scene.enemy())
 
     def update(self) -> None:
         if self.is_game_over():
@@ -27,6 +32,26 @@ class CombatSceneModel(object):
 
     def is_game_over(self) -> bool:
         return IsDead().check(get_player())
+
+    def usable_abilities(self) -> List[Ability]:
+        player = get_player()
+        return [a for a in player.abilities()
+                if any(a.can_use(player, t) for t in self._all_targets)]
+
+    def select_ability(self, index: int) -> None:
+        ability = get_player().abilities()[index]
+        logging.debug('Selected ability ({})'.format(ability.description()))
+        self._selected_ability = ability
+
+    def valid_targets(self) -> Tuple[Character, ...]:
+        if self._selected_ability is None:
+            return ()
+        return tuple(t for t in self._all_targets
+                     if self._selected_ability.can_use(get_player(), t))
+
+    def apply_player_ability(self, target_index) -> None:
+        target = self.valid_targets()[target_index]
+        self._selected_ability.use(get_player(), target)
 
     def _handle_enemy_action(self) -> None:
         action = IncrementAttribute(get_player(), Attribute.HEALTH, -1)
--- src/views/combat_scene_view.py
+++ src/views/combat_scene_view.py
@@ -1,5 +1,7 @@
-from typing import List
+import logging
+from typing import List, Sequence
 
+from models.abilities_base import Ability
 from models.character_base import Character
 from world.world import get_location
 from views.pygame_view import PygameView
@@ -14,6 +16,8 @@ class CombatSceneView(PygameView):
         super(CombatSceneView, self).__init__(
             get_location().background_image_path)
         self.texts: List[str] = None
+        self._targetting_enabled = False
+        self._target_descriptions = []
 
     def render(self) -> None:
         super().render()
@@ -29,12 +33,34 @@ class CombatSceneView(PygameView):
         ]
         return texts
 
-    def _combat_options(self, player: Character) -> List[str]:
-        return ['{} - {}'.format(i, a.description())
-                for i, a in enumerate(player.abilities())]
+    def _combat_options(self,
+                        allowed_abilities: Sequence[Ability]) -> List[str]:
+        return ['{} - {}'.format(i + 1, a.description())
+                for i, a in enumerate(allowed_abilities)]
 
-    def update(self, player: Character, enemy: Character) -> None:
+    def update(self, player: Character, enemy: Character,
+               allowed_abilities: Sequence[Ability]) -> None:
         header = self._scene_description(player, enemy)
-        options = self._combat_options(player)
+        options = self._combat_options(allowed_abilities)
         self.texts = header + options
+        if self._targetting_enabled:
+            self.texts.extend(['', 'Targets:'] + self._target_descriptions)
+
         self.render()
+
+    @property
+    def targetting_enabled(self) -> bool:
+        return self._targetting_enabled
+
+    def enable_targetting(self, targets: Sequence[Character]) -> None:
+        assert not self._targetting_enabled
+        logging.debug('Targetting enabled')
+        self._targetting_enabled = True
+        self._target_descriptions = [
+            '{} - {}'.format(i + 1, t.__class__.__name__)
+            for i, t in enumerate(targets)]
+
+    def disable_targetting(self) -> None:
+        assert self._targetting_enabled
+        logging.debug('Targetting disabled')
+        self._targetting_enabled = False
