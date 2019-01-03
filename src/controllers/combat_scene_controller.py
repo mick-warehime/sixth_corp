fc55add1b414e35081a457de718f5b389872b36e
dvirk <dvir.kafri@gmail.com>
2019-01-03 04:24:44 +0000

    Get targetting working (not using moves).

--- src/controllers/combat_scene_controller.py
+++ src/controllers/combat_scene_controller.py
@@ -4,7 +4,6 @@ from models.combat_scene_model import CombatSceneModel
 from views.combat_scene_view import CombatSceneView
 from controllers.controller import Controller
 from events.events_base import InputEvent, EventType, Event
-NUMERIC_KEYS = [str(i) for i in range(10)]
 
 
 class CombatSceneController(Controller):
@@ -18,11 +17,31 @@ class CombatSceneController(Controller):
         if not self._active:
             return
         if event == Event.TICK:
-            self.view.update(get_player(), self.model.enemy())
+            self.view.update(get_player(), self.model.enemy(),
+                             self.model.usable_abilities())
             self.model.update()
         elif isinstance(event, InputEvent):
             self._handle_input(event)
 
     def _handle_input(self, input_event: InputEvent) -> None:
-        if input_event.key in NUMERIC_KEYS:
-            self.model.try_player_move(int(input_event.key))
+        ability_keys = [str(k + 1) for k in
+                        range(len(self.model.usable_abilities()))]
+        targeting_keys = [str(k + 1) for k in
+                          range(len(self.model.valid_targets()))]
+
+        if self._ability_selected(ability_keys, input_event):
+            self.model.select_ability(int(input_event.key) - 1)
+            self.view.enable_targetting(self.model.valid_targets())
+
+        elif self._target_selected(input_event, targeting_keys):
+            self.view.disable_targetting()
+            self.model.apply_player_ability(int(input_event.key) - 1)
+            self.model._handle_enemy_action()
+
+    def _target_selected(self, input_event, targeting_keys):
+        return (self.view.targetting_enabled
+                and input_event.key in targeting_keys)
+
+    def _ability_selected(self, ability_keys, input_event):
+        return (not self.view.targetting_enabled
+                and input_event.key in ability_keys)
