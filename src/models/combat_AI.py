301d39ce7427eff238d7686e72617a0677be87cf
dvirk <dvir.kafri@gmail.com>
2019-01-03 04:31:56 +0000

    Replace get_moves with valid_moves function.

--- src/models/combat_AI.py
+++ src/models/combat_AI.py
@@ -1,15 +1,24 @@
+from itertools import product
+from typing import Sequence
+
 from models.character_base import Character
 
 
 class Move(object):
     def __init__(self, ability: 'Ability', user: Character,
                  target: Character) -> None:
-        self._ability = ability
+        self.ability = ability
         self._user = user
         self._target = target
 
     def use(self) -> None:
-        self._ability.use(self._user, self._target)
+        self.ability.use(self._user, self._target)
 
     def describe(self) -> str:
-        return self._ability.describe_use(self._user, self._target)
+        return self.ability.describe_use(self._user, self._target)
+
+
+def valid_moves(user: Character,
+                targets: Sequence[Character]) -> Sequence[Move]:
+    return [Move(a, user, t) for a, t in product(user.abilities(), targets)
+            if a.can_use(user, t)]
