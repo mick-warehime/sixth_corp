d3fe118e71d3cd7418bc325637a255f1ba972825
Miiick <mickwarehime@gmail.com>
2019-01-12 11:10:49 -0800

    Refactor AI (#136)
    * refactor ai to not take moves at init
    
    * refactor ai

--- src/combat/ai/random_ai.py
+++ src/combat/ai/random_ai.py
@@ -7,4 +7,6 @@ from combat.moves_base import Move
 class RandomAI(AI):
 
     def select_move(self) -> Move:
-        return choice(self._moves)
+        moves = [m for m in self.moves if m.can_use()]
+        assert len(moves) > 0, 'Random AI has no valid moves'
+        return choice(moves)
--- src/combat/ai/shuffle_ai_test.py
+++ src/combat/ai/shuffle_ai_test.py
@@ -1,37 +1,50 @@
-from collections import defaultdict
 from unittest import TestCase
 
-from characters.ability_examples import FireLaser, Repair
+from characters.ability_examples import Harmless
+from characters.enemies.harmless import HarmlessBuilder
+from characters.enemies.useless import UselessBuilder
 from combat.ai.shuffle_ai import ShuffleAI
-
-REPAIR1 = Repair(1)
-REPAIR2 = Repair(2)
-LASER1 = FireLaser(1)
-LASER2 = FireLaser(2)
-MOVES = [REPAIR1, REPAIR2, LASER1, LASER2]
-
-
-def mock_shuffle_moves():
-    return MOVES.copy()
 
 
 class ShuffleAITest(TestCase):
 
-    def test_all_moves_get_used(self):
-        ai = ShuffleAI(MOVES)
-        ai.shuffle = mock_shuffle_moves
-        for move in MOVES:
-            actual_move = ai.select_move()
-            self.assertEqual(actual_move, move)
+    def test_shuffle_ai_only_provides_usable_moves(self):
+        user = HarmlessBuilder().build()
+        target = HarmlessBuilder().build()
+        ai = ShuffleAI(user=user)
+        ai.set_targets([target])
 
-    def test_moves_get_used_correct_number_of_times(self):
-        n_shuffles = 3
-        n_moves = len(MOVES)
-        ai = ShuffleAI(MOVES, shuffle_size=n_shuffles)
-        move_count = defaultdict(int)
-        for _ in range(n_moves * n_shuffles):
+        for i in range(1000):
             move = ai.select_move()
-            move_count[move] += 1
+            self.assertIsInstance(move.ability, Harmless)
 
-        for move in move_count:
-            self.assertEqual(move_count[move], n_shuffles)
+    def test_shuffle_ai_moves_dont_repeat(self):
+        user = HarmlessBuilder().build()
+        target = HarmlessBuilder().build()
+        ai = ShuffleAI(user=user)
+        ai.set_targets([target])
+
+        prev_move_description = ''
+        move_repeat_count = 0
+        for i in range(10000):
+            move = ai.select_move()
+            move_description = move.describe()
+            if move_description == prev_move_description:
+                move_repeat_count += 1
+            else:
+                move_repeat_count = 0
+            prev_move_description = move_description
+
+            # the most a move can repeat is 2 times - assuming unique moves
+            # and all moves get played before reshuffle
+            # [1,2] -> [2, 1] -> [1, 2]
+            self.assertLess(move_repeat_count, 3)
+
+    def test_no_valid_moves_raises(self):
+        user = UselessBuilder().build()
+        target = UselessBuilder().build()
+        ai = ShuffleAI(user=user)
+        ai.set_targets([target])
+
+        with self.assertRaises(AssertionError):
+            ai.select_move()
