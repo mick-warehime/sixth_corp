9c69bea5d5a585de70e4e3fa7041d2be84c968f2
dvirk <dvir.kafri@gmail.com>
2019-01-12 23:54:42 +0000

    Add a temporary dummy HUMAN AIType.

--- src/combat/ai/ai_factory.py
+++ src/combat/ai/ai_factory.py
@@ -17,5 +17,7 @@ def build_ai(character: Character, ai_type: AIType) -> AI:
         return RandomAI(character)
     elif ai_type == AIType.Shuffle:
         return ShuffleAI(character)
+    elif ai_type == AIType.Human:
+        return None  # type: ignore
 
     raise ValueError('Unexpected AIType {}'.format(ai_type))
