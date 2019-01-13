e8f66a16785eb47881160498725e654d1fa4c96e
dvirk <dvir.kafri@gmail.com>
2019-01-13 00:23:05 +0000

    Remove obsolete CharacterBuilder

--- src/characters/character_builder.py
+++ src/characters/character_builder.py
@@ -1,67 +1,13 @@
-from abc import abstractmethod
 from enum import Enum
-from typing import Any, Dict, Sequence
+from typing import Dict
 
 from characters.character_base import Character
 from characters.character_examples import (DRONE, HARMLESS, HUMAN_PLAYER,
                                            USLESS, CharacterData)
-from characters.chassis import Chassis
 from characters.enemy_base import Enemy
-from characters.mods_base import GenericMod, Mod
+from characters.mods_base import GenericMod
 from characters.states import Attribute
 from combat.ai.ai_factory import build_ai
-
-
-class CharacterBuilder(object):
-    """Abstract class used to simplify enumerating players and enemies."""
-
-    def base_class(self) -> Any:
-        return Character
-
-    @abstractmethod
-    def initial_mods(self) -> Sequence[Mod]:
-        return []
-
-    @abstractmethod
-    def chassis(self) -> Chassis:
-        return None
-
-    @abstractmethod
-    def additional_attributes(self) -> Dict[Attribute, int]:
-        return {}
-
-    @abstractmethod
-    def max_health(self) -> int:
-        return 0
-
-    @abstractmethod
-    def image_path(self) -> str:
-        return ''
-
-    @abstractmethod
-    def character_name(self) -> str:
-        return ''
-
-    def build(self) -> Character:
-        character_cls = self.base_class()
-        char = character_cls(
-            health=self.max_health(),
-            image_path=self.image_path(),
-            name=self.character_name())
-        for mod in self.initial_mods():
-            char.attempt_pickup(mod)
-
-        attr_dict = self.additional_attributes()
-        for attr in attr_dict:
-            val = attr_dict[attr]
-            char.increment_attribute(attr, val)
-
-        # TODO(mick) - move positions to combat view
-        char.set_position(200, 500, 150, 150)
-
-        # TODO(dvirk) - set chassis for character
-
-        return char
 
 
 class CharacterFactory(Enum):
