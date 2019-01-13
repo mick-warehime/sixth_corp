85ec1e4f6c8512b0634f96fd635dd979ae55cd14
dvirk <dvir.kafri@gmail.com>
2019-01-13 00:02:20 +0000

    Remove references to UselessBuilder

--- src/combat/ai/ai_test.py
+++ src/combat/ai/ai_test.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 
 from parameterized import parameterized
 
-from characters.enemies.useless import UselessBuilder
+from characters.character_builder import CharacterFactory
 from combat.ai.ai_factory import AIType, build_ai
 
 AI_TYPES = [[AIType.Random], [AIType.Shuffle]]
@@ -13,9 +13,9 @@ class AITest(TestCase):
 
     @parameterized.expand(AI_TYPES)
     def test_no_valid_moves_raises(self, ai_type):
-        user = UselessBuilder().build()
+        user = CharacterFactory.USELESS.build()
         ai = build_ai(user, ai_type)
-        target = UselessBuilder().build()
+        target = CharacterFactory.USELESS.build()
         ai.set_targets([target])
 
         with self.assertRaises(AssertionError):
