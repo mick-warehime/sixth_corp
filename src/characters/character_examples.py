9792a9afb717fb2d2962f7b249147a05b878e53d
dvirk <dvir.kafri@gmail.com>
2019-01-13 21:42:23 +0000

    remove character building from CharacterTypes

--- src/characters/character_examples.py
+++ src/characters/character_examples.py
@@ -1,14 +1,9 @@
 """Data class and examples for enemies."""
 from enum import Enum
-from typing import NamedTuple, Dict
-
-from characters.character_impl import CharacterImpl
-from characters.character_position import Position
+from typing import NamedTuple
 
 from characters.chassis_examples import ChassisTypes
-from characters.mods_base import GenericMod
-from characters.states import Attribute
-from combat.ai.ai_factory import AIType, build_ai
+from combat.ai.ai_factory import AIType
 
 CharacterData = NamedTuple(
     'EnemyData', [('name', str),
@@ -34,27 +29,6 @@ class CharacterTypes(Enum):
     HARMLESS = _HARMLESS
     USELESS = _USELESS
 
-    def build(self) -> CharacterImpl:
-        data = self.value
-
-        # This is an intermediate fix as in future these things will be handled
-        # by the chassis.
-        chassis = data.chassis_type.build()
-        max_health = chassis.total_modifier(Attribute.MAX_HEALTH)
-
-        char = CharacterImpl(health=max_health,
-                             image_path=data.image_path,
-                             name=data.name)
-
-        temp_mod = GenericMod(abilities_granted=chassis.all_abilities())
-        char.attempt_pickup(temp_mod)
-
-        # TODO(#112) - move positions to combat view
-        if self == CharacterTypes.HUMAN_PLAYER:
-            pos = Position(200, 500, 150, 150)
-        else:
-            pos = Position(800, 300, 200, 150)
-        char.position = pos
-        char.ai = build_ai(char, data.ai_type)
-
-        return char
+    @property
+    def data(self) -> CharacterData:
+        return self.value
--- src/characters/character_factory.py
+++ src/characters/character_factory.py
@@ -1,3 +1,35 @@
+from characters.character_examples import CharacterTypes
+from characters.character_impl import CharacterImpl
+from characters.character_position import Position
+from characters.mods_base import GenericMod
+from characters.states import Attribute
+from combat.ai.ai_factory import AIType, build_ai
 
 
+def build_character(character) -> CharacterImpl:
+    if isinstance(character, CharacterTypes):
+        data = character.data
+    else:
+        data = character
 
+    # This is an intermediate fix as in future these things will be handled
+    # by the chassis.
+    chassis = data.chassis_type.build()
+    max_health = chassis.total_modifier(Attribute.MAX_HEALTH)
+
+    char = CharacterImpl(health=max_health,
+                         image_path=data.image_path,
+                         name=data.name)
+
+    temp_mod = GenericMod(abilities_granted=chassis.all_abilities())
+    char.attempt_pickup(temp_mod)
+
+    # TODO(#112) - move positions to combat view
+    if data.ai_type == AIType.Human:
+        pos = Position(200, 500, 150, 150)
+    else:
+        pos = Position(800, 300, 200, 150)
+    char.position = pos
+    char.ai = build_ai(char, data.ai_type)
+
+    return char
