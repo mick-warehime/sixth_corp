e1c4fe0ab84f091215dc38b7f42d78ea7dfd17c3
Miiick <mickwarehime@gmail.com>
2019-01-13 20:04:43 -0800

    reduce settings redrawing

--- src/controllers/settings_controller.py
+++ src/controllers/settings_controller.py
@@ -8,9 +8,13 @@ class SettingsController(Controller):
     def __init__(self) -> None:
         super(SettingsController, self).__init__()
         self.view = SettingsView()
+        self.update()
 
     def notify(self, event: EventType) -> None:
         if not self._active:
             return
-        if event == Event.TICK:
-            self.view.render()
+        if event != Event.TICK:
+            self.update()
+
+    def update(self) -> None:
+        self.view.render()
