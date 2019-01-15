fa72e161bc2532ba9be22d58e63800329cc0e21c
Miiick <mickwarehime@gmail.com>
2019-01-14 21:35:20 -0800

    Status overlay (#174)
    * add basic status overlay
    
    * make locations event listeners to track current scene
    
    * ensure world/location is instantiated at start of game

--- src/views/pygame_view.py
+++ src/views/pygame_view.py
@@ -1,10 +1,19 @@
-from typing import Iterable
+from typing import Dict, Iterable, List
 
 import pygame
 from pygame import Surface
 
+from characters.player import get_player
+from characters.states import Attribute
 from data import constants
 from views.pygame_images import load_image
+from world.world import get_location
+
+DARK_GRAY = [50, 50, 50]
+RED = [255, 0, 0]
+GREEN = [0, 255, 0]
+BLUE = [0, 0, 255]
+WHITE = [255, 255, 255]
 
 
 class PygameView(object):
@@ -13,8 +22,16 @@ class PygameView(object):
     def __init__(self, background_image_path: str) -> None:
         if self.screen is None:
             self._initialize_screen()
-        self._smallfont = pygame.font.Font(None, 40)
         self._background_path = background_image_path
+        self._fonts: Dict[int, pygame.font.Font] = {}
+
+    def font(self, size: int) -> pygame.font.Font:
+        if size not in self._fonts:
+            font = pygame.font.Font(None, size)
+            self._fonts[size] = font
+        else:
+            font = self._fonts[size]
+        return font
 
     def _initialize_screen(self) -> None:
         pygame.display.set_caption('6th Corp')
@@ -30,17 +47,24 @@ class PygameView(object):
 
     def render_text(self, texts: Iterable[str]) -> None:
         offset = 0
+        x = 250
         for text in texts:
-            rasterized = self._smallfont.render(text, True, (0, 255, 0))
-            self.screen.blit(rasterized, (250, 250 + offset))
+            y = 250 + offset
+            self.render_font(text, 40, x, y, GREEN)
             offset += 50
         self.update_display()
+
+    def render_font(self, text: str, size: int, x: int, y: int, color: List[int]) -> None:
+        font = self.font(size)
+        rasterized = font.render(text, True, color)
+        self.screen.blit(rasterized, (x, y))
 
     def update_display(self) -> None:
         pygame.display.flip()
 
     def render_background(self, image_path: str) -> None:
         self.render_image(image_path, 0, 0)
+        self.render_overlay()
 
     def render_image(self, image_path: str, x: int, y: int, w: int = 0, h: int = 0) -> None:
         image = load_image(image_path)
@@ -53,5 +77,27 @@ class PygameView(object):
         self.update_display()
 
     def draw_rect(self, x: int, y: int, w: int, h: int) -> None:
-        pygame.draw.rect(self.screen, [250, 0, 0], pygame.rect.Rect(x, y, w, h), 2)
+        pygame.draw.rect(self.screen, RED, pygame.rect.Rect(x, y, w, h), 2)
         self.update_display()
+
+    def render_overlay(self) -> None:
+
+        # draw gray background
+        height = 40
+        width = constants.SCREEN_SIZE[0]
+        pygame.draw.rect(self.screen, DARK_GRAY, pygame.rect.Rect(0, 0, width, height))
+
+        # draw health
+        player = get_player()
+        health = player.get_attribute(Attribute.HEALTH)
+        max_health = player.get_attribute(Attribute.MAX_HEALTH)
+        health_text = 'Health: {} / {}'.format(health, max_health)
+        x_health = int(width / 2 - 300)
+        y_health = int(height / 2 - 5)
+        self.render_font(health_text, 21, x_health, y_health, RED)
+
+        # draw scene number
+        scene_text = 'Scene: {}'.format(get_location().scene)
+        x_scene = int(width / 2 + 100)
+        y_scene = int(height / 2 - 5)
+        self.render_font(scene_text, 22, x_scene, y_scene, WHITE)
--- src/world/location_base.py
+++ src/world/location_base.py
@@ -1,11 +1,19 @@
+from events.events_base import EventListener, EventType, NewSceneEvent
+
 """Class that represents the current location (sights, sounds, flavors)."""
 
 
-class Location(object):
+class Location(EventListener):
 
     def __init__(self, background_image_path: str) -> None:
+        super().__init__()
         self._background_image_path = background_image_path
+        self.scene = 0
 
     @property
     def background_image_path(self) -> str:
         return self._background_image_path
+
+    def notify(self, event: EventType) -> None:
+        if isinstance(event, NewSceneEvent):
+            self.scene += 1
