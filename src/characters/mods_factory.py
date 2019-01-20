37167f9601c1140940315070d59e5e915b436c3e
dvirk <dvir.kafri@gmail.com>
2019-01-20 02:35:01 +0000

    mod factory now uses slot data

--- src/characters/mods_factory.py
+++ src/characters/mods_factory.py
@@ -3,4 +3,4 @@ from characters.mods_base import GenericMod, ModData
 
 def build_mod(mod: ModData) -> GenericMod:
     return GenericMod(mod.states_granted, mod.attribute_modifiers,
-                      mod.abilities_granted)
+                      mod.abilities_granted, mod.valid_slots)
