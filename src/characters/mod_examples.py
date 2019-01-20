3fefba1130f3be2e8e1f720c12eeda046aba52d4
dvirk <dvir.kafri@gmail.com>
2019-01-20 02:38:48 +0000

    assign slots to example mods.

--- src/characters/mod_examples.py
+++ src/characters/mod_examples.py
@@ -1,17 +1,13 @@
 """Simple example mods."""
 from enum import Enum
-from typing import Dict, Sequence
-
-from characters.abilities_base import Ability
 from characters.ability_examples import FireLaser
-from characters.mods_base import Mod, ModData
-from characters.states import Attribute, AttributeType, Skill, State
+from characters.mods_base import ModData, Slots
+from characters.states import Attribute, Skill, State
 
 
 class ModTypes(Enum):
     BASIC_HULL_PLATING = 'hull plating'
     FIRE_HELM = 'helm of being on fire'
-    SLEEPY_AMULET = 'amulet of sleepiness'
     CAMOUFLAGE_PAINT = 'camo paint'
     SMALL_LASER = 'small laser'
     BIG_LASER = 'big laser'
@@ -23,11 +19,15 @@ class ModTypes(Enum):
 
 _mod_types_to_data = {
     ModTypes.BASIC_HULL_PLATING: ModData(
-        attribute_modifiers={Attribute.MAX_HEALTH: 3}),
-    ModTypes.FIRE_HELM: ModData(states_granted=(State.ON_FIRE,)),
-    ModTypes.SLEEPY_AMULET: ModData(states_granted=(State.SLEEPY,)),
-    ModTypes.CAMOUFLAGE_PAINT: ModData(attribute_modifiers={Skill.STEALTH: 1}),
-    ModTypes.SMALL_LASER: ModData(abilities_granted=(FireLaser(2),)),
-    ModTypes.BIG_LASER: ModData(abilities_granted=(FireLaser(4),))
+        attribute_modifiers={Attribute.MAX_HEALTH: 3},
+        valid_slots=(Slots.CHEST,)),
+    ModTypes.FIRE_HELM: ModData(states_granted=(State.ON_FIRE,),
+                                valid_slots=(Slots.HEAD,)),
+    ModTypes.CAMOUFLAGE_PAINT: ModData(attribute_modifiers={Skill.STEALTH: 1},
+                                       valid_slots=(Slots.CHEST,)),
+    ModTypes.SMALL_LASER: ModData(abilities_granted=(FireLaser(2),),
+                                  valid_slots=(Slots.ARMS,)),
+    ModTypes.BIG_LASER: ModData(abilities_granted=(FireLaser(4),),
+                                valid_slots=(Slots.ARMS,))
 
 }
