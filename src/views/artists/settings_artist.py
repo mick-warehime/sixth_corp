b20d054530f95740e77e34daec0a61a99fe78e8d
dvirk <dvir.kafri@gmail.com>
2019-02-01 04:58:23 +0000

    rescale settings_artist

--- src/views/artists/settings_artist.py
+++ src/views/artists/settings_artist.py
@@ -1,5 +1,6 @@
 from data.colors import WHITE
 from scenes.settings_scene import SettingsScene
+from views.artists.drawing_utils import rescale_horizontal, rescale_vertical
 from views.artists.scene_artist_base import SceneArtist
 from views.screen_base import Screen
 
@@ -8,12 +9,14 @@ class SettingsArtist(SceneArtist):
 
     def render(self, screen: Screen, scene: SettingsScene) -> None:
         background_image = scene.background
+
+        x, font_size, spacing = rescale_horizontal(250, 35, 50)
+        y = rescale_vertical(250)
         screen.render_image(background_image, x=0, y=0, h=0, w=0)
         screen.render_texts(
-            list(
-                scene.options),
-            font_size=35,
-            x=250,
-            y=250,
+            list(scene.options),
+            font_size=font_size,
+            x=x,
+            y=y,
             color=WHITE,
-            spacing=50)
+            spacing=spacing)
