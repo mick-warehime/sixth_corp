6acbb2adca4200d78a9e1cb6d4e65cca765ce07a
dvirk <dvir.kafri@gmail.com>
2019-04-19 17:44:42 +0000

    Overlay no longer shows player health

--- src/views/artists/overlay_artist.py
+++ src/views/artists/overlay_artist.py
@@ -1,9 +1,4 @@
-from pygame.rect import Rect
-
-from data import constants
-from data.colors import DARK_GRAY, RED, WHITE
-from models.characters.player import get_player
-from models.characters.states import Attributes
+from data.colors import WHITE
 from models.scenes.scenes_base import Scene
 from views.artists.scene_artist_base import SceneArtist
 from views.pygame_screen import Screen
@@ -13,20 +8,6 @@ class OverlayArtist(SceneArtist):
     """Draws basic overlay information, like player health."""
 
     def render(self, screen: Screen, scene: Scene) -> None:
-        # draw gray background
-        height = 40
-        width = constants.SCREEN_SIZE[0]
-        screen.render_rect(Rect(0, 0, width, height), DARK_GRAY, 0)
-
-        # draw health
-        player = get_player()
-        health = player.status.get_attribute(Attributes.HEALTH)
-        max_health = player.status.get_attribute(Attributes.MAX_HEALTH)
-        health_text = 'Health: {} / {}'.format(health, max_health)
-        x_health = int(width / 2 - 300)
-        y_health = int(height / 2 - 5)
-        screen.render_text(health_text, font_size=21, x=x_health, y=y_health,
-                           color=RED)
 
         # key hints
         screen.render_text('i: Inventory', 28, 20, 50, WHITE)
