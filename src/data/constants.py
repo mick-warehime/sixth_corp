ba45476dfc130047d399b0d6a678d5b23937114c
dvirk <dvir.kafri@gmail.com>
2019-04-24 14:32:19 -0700

    Change default screen size.

--- src/data/constants.py
+++ src/data/constants.py
@@ -4,7 +4,7 @@ FRAMES_PER_SECOND = 30
 LOG_LEVEL = 'DEBUG'
 LOGGING_FILE = 'src/logging/game.log'
 PREFERENCES_FILE = 'src/data/bindings.pref'
-SCREEN_SIZE = (1600, 1200)
+SCREEN_SIZE = (1200, 800)
 TEXTWIDTH = 68
 VERSION = 0.001
 
