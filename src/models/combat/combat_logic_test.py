181d457adf771c4daa80422017cab2ebce348ca3
dvirk <dvir.kafri@gmail.com>
2019-04-28 19:08:06 +0000

    add test of basic laser move

--- src/models/combat/combat_logic_test.py
+++ src/models/combat/combat_logic_test.py
@@ -2,20 +2,20 @@ import pytest
 
 from models.characters.character_examples import CharacterTypes
 from models.characters.character_impl import build_character
-from models.characters.player import reset_player, get_player
+from models.characters.chassis import Chassis
+from models.characters.chassis_examples import ChassisTypes
+from models.characters.mods_base import build_mod
+from models.characters.moves_base import Move
 from models.characters.states import Attributes
+from models.characters.subroutine_examples import direct_damage
 from models.combat.combat_logic import CombatLogic
 
 
 @pytest.fixture()
-def player_char():
-    reset_player()
-    # remove all mods
-    player = get_player()
-    for mod in player.chassis.all_mods():
-        player.chassis.remove_mod(mod)
-
-    return player
+def player():
+    # Player character with zero initial mods.
+    chassis = Chassis.from_data(ChassisTypes.NO_LEGS.data)
+    return build_character(chassis, name='player')
 
 
 @pytest.fixture()
@@ -23,8 +23,8 @@ def enemy():
     return build_character(data=CharacterTypes.DRONE.data)
 
 
-def test_combat_logic_initializes_cpu(player_char, enemy):
-    chars = [player_char, enemy]
+def test_combat_logic_initializes_cpu(player, enemy):
+    chars = [player, enemy]
     for char in chars:
         cpu = char.status.get_attribute(Attributes.CPU_AVAILABLE)
         char.status.increment_attribute(Attributes.CPU_AVAILABLE, -cpu)
@@ -35,3 +35,35 @@ def test_combat_logic_initializes_cpu(player_char, enemy):
         actual = char.status.get_attribute(Attributes.CPU_AVAILABLE)
         expected = char.status.get_attribute(Attributes.MAX_CPU)
         assert actual == expected
+
+
+def test_direct_move_removes_and_returns_cpu(player, enemy):
+    cpu_used = 2
+    time = 3
+    damage = 1
+    move = Move(direct_damage(damage, cpu_slots=cpu_used, time_to_resolve=time),
+                player, enemy)
+
+    logic = CombatLogic([player, enemy])
+
+    def get_cpu():
+        return player.status.get_attribute(Attributes.CPU_AVAILABLE)
+
+    def get_health():
+        return enemy.status.get_attribute(Attributes.HEALTH)
+
+    starting_cpu = get_cpu()
+    starting_HP = get_health()
+    # Add move to stack and wait until it resolves.
+    logic.start_round([move])
+    for _ in range(time):
+        assert get_cpu() == starting_cpu - cpu_used
+        assert get_health() == starting_HP
+        logic.end_round()
+        assert get_cpu() == starting_cpu - cpu_used
+        logic.start_round([])
+
+    # When it resolves cpu should return and HP should go down.
+    logic.end_round()
+    assert get_cpu() == starting_cpu
+    assert get_health() == starting_HP - damage
